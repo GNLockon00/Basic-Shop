afd79df8e31ecaa0b2c58c8859447da2d714a6f6
GNLockon00 <45247277+GNLockon00@users.noreply.github.com>
2018-11-23 16:44:48 +0000

    Add files via upload

---
+++ Shop/src/Items.java
@@ -0,0 +1,26 @@
+public class Items {
+
+    private int id;
+    private String Name;
+    private int Price;
+
+    public Item(int itemID, String ItemName, int ItemPrice){
+        id=itemID;
+        Name=ItemName;
+        Price=ItemPrice;
+    }
+
+    public int getID{
+        return id;
+    }
+
+    public String getName{
+        return Name;
+    }
+
+    public int getPrice{
+        return Price;
+    }
+
+
+}
